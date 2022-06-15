d625b88fbb4466dc0fb9e41fc47f5a6b41cb8cca
Roald Ong <roald.ong@gmail.com>
2022-06-15 18:04:12 +0800

    Added in service call to control lights

--- include/gzpluginz/RosLedPlugin.h
+++ include/gzpluginz/RosLedPlugin.h
@@ -3,6 +3,7 @@
 #include <gazebo/plugins/LedPlugin.hh>
 #include <iostream>
 #include <ros/ros.h>
+#include <std_srvs/SetBool.h>
 
 namespace gazebo {
 
@@ -10,6 +11,13 @@ class RosLed : public LedPlugin {
 
 public:
   void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf) override;
+
+  bool Control(std_srvs::SetBool::Request &_req,
+               std_srvs::SetBool::Response &_res);
+
+  /// \brief ROS service server.
+private:
+  ros::ServiceServer service;
 };
 
 GZ_REGISTER_MODEL_PLUGIN(RosLed)
--- src/RosLed.cc
+++ src/RosLed.cc
@@ -4,10 +4,34 @@ namespace gazebo {
 
 void RosLed::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf) {
 
+  // Make sure the ROS node for Gazebo has already been initialized
+  if (!ros::isInitialized()) {
+    ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable "
+                     "to load plugin. "
+                     << "Load the Gazebo system plugin "
+                        "'libgazebo_ros_api_plugin.so' in the gazebo_ros "
+                        "package)");
+    return;
+  }
+  ROS_INFO("RosLed Gazebo ROS plugin loading.");
+
   LedPlugin::Load(_parent, _sdf);
   std::cout << "YESSSS\n";
-  this->TurnOnAll();
-  // this->TurnOffAll();
+
+  // ROS service to receive a command to control the light
+  ros::NodeHandle n;
+  this->service = n.advertiseService("lights", &RosLed::Control, this);
+}
+
+bool RosLed::Control(std_srvs::SetBool::Request &_req,
+                     std_srvs::SetBool::Response &_res) {
+  if (_req.data) {
+    _res.success = this->TurnOnAll();
+  } else {
+    _res.success = this->TurnOffAll();
+  }
+
+  return _res.success;
 }
 
 } // namespace gazebo
